ef4827647f1400fad8b2811bf2f46d016add87b7
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2022-07-03 23:54:10 +0900

    Add files via upload

--- slideshow.js
+++ slideshow.js
@@ -1,6 +1,6 @@
 var slides = document.querySelectorAll("#slides>img")
 var prev = document.getElementById("prev")
-var prev = document.getElementById("next")
+var next = document.getElementById("next")
 
 var current = 0;
 showSlides(current) // 현재 이미지 표시
@@ -16,9 +16,9 @@ function showSlides(n){
 }
 
 function prevSlide(){
-    if (current>0) {
+    if (current > 0) {
         current -= 1 // 이전 이미지를 보여줌
-    } else {
+    } else { // current = 0일 때
         current = slides.length - 1 
         // 첫번째 이미지에서 이전 이미지는 맨 마지막 이미지임.
         // 맨 마지막 이미지의 인덱스는 총 길이 - 1
