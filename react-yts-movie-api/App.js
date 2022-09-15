e3047ceb759a093929ba74cbc8e09ef225737776
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2022-09-15 17:13:27 +0900

    Add files via upload

--- react-yts-movie-api/App.js
+++ react-yts-movie-api/App.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import { Component } from 'react';
 import './App.css';
+import Movie from './components/Movie.js'
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state={
-      movieList:[] // 빈 배열
+      movieTitleList:[] // 빈 배열
     }
   }
 
@@ -19,7 +20,7 @@ class App extends Component {
     = await axios.get("https://yts.mx/api/v2/list_movies.json")
     this.setState({movies})
 
-    const {movieList} = this.state
+    const {movieTitleList} = this.state
 
     for(var i=0; i<movies.length; i++){
         // finalMovieList: [...movieList, movies[i].title]
@@ -27,7 +28,9 @@ class App extends Component {
         // push말고 원래 배열에 값 계속 추가 하는 함수?
         // concat은 제일 마지막 배열만 저장됨
         // this.setState 할 때마다 값이 변경됨!
-      movieList.push(movies[i].title)
+        movieTitleList.push(movies[i].title)
+      console.log(movies[i])
+      // movieList가 필요가 있나??
     }
   }
 
@@ -35,11 +38,12 @@ class App extends Component {
 
     return (
       <div id="App">
-        <p>{this.state.movieList[0]}</p>
-        <p>{this.state.movieList[1]}</p>
-        <p>{this.state.movieList[2]}</p>
-        <p>{this.state.movieList[3]}</p>
-        <p>{this.state.movieList[4]}</p>
+        <p>{this.state.movieTitleList[0]}</p>
+        <p>{this.state.movieTitleList[1]}</p>
+        <p>{this.state.movieTitleList[2]}</p>
+        <p>{this.state.movieTitleList[3]}</p>
+        <p>{this.state.movieTitleList[4]}</p>
+        <Movie movieTitleList={this.state.movieTitleList}/>
       </div>
     );
   }
