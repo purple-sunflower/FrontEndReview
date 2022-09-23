04c7fab06050981e4964b2cafc372c8a2dfdef4e
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2022-09-23 20:37:58 +0900

    Add files via upload

--- react-hook-full-stack/server/config/db.js
+++ react-hook-full-stack/server/config/db.js
@@ -1,7 +1,9 @@
-var mysql = require('mysql')
+var mysql=require('mysql')
 const db=mysql.createPool({
     host:'localhost',
     user:'root',
     password:'1234',
     database:'node_db',
+    port:3306
 })
+module.exports=db;
