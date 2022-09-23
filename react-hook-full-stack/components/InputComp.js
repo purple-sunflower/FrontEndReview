c87437131727088eb6923b96259f819c479c080c
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2022-09-23 20:36:58 +0900

    Add files via upload

--- react-hook-full-stack/components/InputComp.js
+++ react-hook-full-stack/components/InputComp.js
@@ -1,36 +1,37 @@
-import { useState } from "react";
-import "../css/InputComp.css"
+import '../css/InputComp.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function InputComp(props) {
-  
-    const [name, setName] = useState('')
-    const [age, setAge] = useState('')
-    const [height, setHeight] = useState('')
 
-    const addPersonInfo=()=>{
-        props.addPersonInfo(name, age, height)
-    }
+  const [name, setName] = useState('')
+  const [age, setAge] = useState('')
+  const [height, setHeight] = useState('')
 
-    const inputChangeName = (e) =>{
-        setName(e.target.value)
-    }
-
-    const inputChangeAge = (e) =>{
-        setAge(e.target.value)
-    }
-
-    const inputChangeHeight = (e) =>{
-        setHeight(e.target.value)
-    }
-
-    return (
-      <div id="input-comp">
-        <input type='text' onChange={inputChangeName} placeholder='이름'/>
-        <input type='text' onChange={inputChangeAge} placeholder='나이'/>
-        <input type='text' onChange={inputChangeHeight} placeholder='키'/>
-        <button onClick={addPersonInfo}>추가</button>
-      </div>
-    );
+  const addPersonInfo=()=>{
+    props.addPersonInfo(name, age, height)
   }
-  
-  export default InputComp;
+
+  const inputChangeName = (e) =>{
+    setName(e.target.value)
+  }
+
+  const inputChangeAge = (e) =>{
+    setAge(e.target.value)
+  }
+
+  const inputChangeHeight = (e) =>{
+    setHeight(e.target.value)
+  }
+
+  return (
+    <div id='inputcomp'>
+      <input type="text" onChange={inputChangeName} placeholder="이름"/>
+      <input type="text" onChange={inputChangeAge} placeholder="나이"/>
+      <input type="text" onChange={inputChangeHeight} placeholder="키"/>
+      <button onClick={addPersonInfo}>추가</button>
+    </div>
+  );
+}
+
+export default InputComp;
