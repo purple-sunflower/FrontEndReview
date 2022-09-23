e3e8cb6fcda570dc2131222b68f64b334e77c923
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2022-09-23 19:10:52 +0900

    Add files via upload

---
+++ naver-api-search-router/components/Detail.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+import '../css/Detail.css';
+
+class Detail extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+    
+    }
+  }
+
+  render(){
+    return (
+      <div id="detail">
+        상세보기
+      </div>
+    );
+  }
+}
+
+export default Detail;
