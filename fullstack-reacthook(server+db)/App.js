2da0b93880643d72723081c5012b7070aff59ffe
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2022-09-27 21:37:38 +0900

    Add files via upload

--- fullstack-reacthook(server+db)/App.js
+++ fullstack-reacthook(server+db)/App.js
@@ -1,40 +1,18 @@
-import { useState, useEffect } from 'react';
+
 import './App.css';
-import axios from 'axios';
-import PostList from './components/PostList.js';
-import Pagination from './components/Pagination.js'
-// 번호, 제목, 작성자, 등록일, 첨부, 조회 , (글 내용)\
+// 번호, 제목, 작성자, 등록일, 첨부, 조회 , (글 내용)
 // no, title, author, date, , hits, contents
+// 글쓰기 router 사용 (postWrite)
+// postlist를 postmain으로 옮기고
+// app js에 라우터 써야할 듯!
 
 
 function App() {
 
-  const [postList, setPostList] = useState([]);
-
-  useEffect(()=>{
-    const getPosts = async() =>{
-      const result = await axios.get('/board')
-      setPostList(result.data)
-    }
-    getPosts();
-  })
-
-  const [postPerPage, setPostPerPage] = useState(10)
-  const [currentPage, setCurrentPage] = useState(1)
-
-  const currentPostList=(postList)=>{
-    const indexOfFirst = (currentPage-1)*postPerPage
-    const indexOfLast = indexOfFirst+postPerPage
-    const slicedPostList = postList.slice(indexOfFirst, indexOfLast)
-    return slicedPostList;
-
-  }
-
+  
   return (
     <div id='app'>
-      <PostList postList={currentPostList(postList)}/>
-      <Pagination currentPage={currentPage} postPerPage={postPerPage} 
-      total={postList.length} setCurrentPage={setCurrentPage}/>
+      
     </div>
   );
 }
