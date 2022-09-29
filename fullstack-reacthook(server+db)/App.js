f60ab957e2f81f2e123342bce6a698089c741877
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2022-09-29 21:40:24 +0900

    Add files via upload

--- fullstack-reacthook(server+db)/App.js
+++ fullstack-reacthook(server+db)/App.js
@@ -1,13 +1,19 @@
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import PostMain from './components/PostMain.js'
+import PostWrite from './components/PostWrite.js'
+import PostView from './components/PostView.js'
+import Search from './components/Search.js'
+
 // 번호, 제목, 작성자, 등록일, 첨부, 조회 , (글 내용)
 // no, title, author, date, , hits, contents
-// 글쓰기 router 사용 (postWrite)
-// postlist를 postmain으로 옮기고
-// app js에 라우터 써야할 듯!
+// react-quill 이용해서 글쓰기 에디터 만들기! postWrite
 
+// posts={posts}
+// enrollPost={enrollPost}
+
+// Search, SearchComp, server 수정하기!! (검색해서 결과 나오도록..)
 
 function App() {
 
@@ -15,10 +21,14 @@ function App() {
     <div id='app'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<PostMain/>}/>
+          <Route exact path='/' element={<PostMain/>}/>
+          <Route path='/postWrite' element={<PostWrite/>}/>
+          <Route path='/postView' element={<PostView/>}/>
+          <Route path='/search' element={<Search/>}/>
         </Routes>
       </BrowserRouter>
     </div>
+    
   );
 }
 
